1927ed290561242911b5f79f95db92a637c1ce86
Kojima Seiichi <kojima.seiichi@outlook.jp>
2017-12-08 20:20:18 +0900

    hoge

--- create-database.sql
+++ create-database.sql
@@ -1,34 +1,58 @@
-create database tsuta;
-create user tsutap@localhost identified by tsutap;
-grant all on tsuta.* to tsutap@localhost;
+drop database tsuta;
+drop user tsuta@localhost;
 
-create table user (
+create database tsuta;
+create user 'tsuta'@localhost identified by 'tsuta';
+grant all on tsuta.* to tsuta@localhost;
+
+use tsuta;
+
+create table islander (
 	id int primary key auto_increment,
 	first_name varchar(30) not null,
 	last_name varchar(30) not null,
 	mail varchar(300),
-	account_id int
-);
-
-create table role (
-	id int primary key auto_increment,
-	name varchar(30) not null
+	sex char(1)
 );
 
 create table account (
 	id int primary key auto_increment,
-	user_name varchar(90) not null,
-	password varchar(90) not null,
-	locked boolean not null,
-	constraint account_uq_user_name unique (user_name)
+	secret_id int,
+	display_name varchar(90) not null,
+	disabled char(1) not null,
+	constraint uq_account_display_name unique (display_name)
+);
+
+create table secret (
+	id int primary key auto_increment,
+	secret varchar(1000) not null
+);
+
+create table role (
+	id int primary key auto_increment,
+	name varchar(30) not null,
+	individual_account_id int,
+	constraint uq_role_name unique (name)
 );
 
 -- 
 create table user_role (
-	user_id int not null,
-	role_id int not null,
-	constraint user_role_pk primary key (user_id, role_id),
-	constraint user_role_fk1 foreign key (user_id) references user(id) on update no action on delete no action,
-	constraint user_role_fk2 foreign key (role_id) references role(id) on update no action on delete no action
+	user_id int,
+	role_id int,
+	constraint pk_user_role primary key (user_id, role_id)
 	
 );
+
+create table resource (
+	id int primary key auto_increment,
+	subtype varchar(30) not null,
+	uri varchar(1000) not null,
+	express varchar(1000)
+);
+
+create table role_resource (
+	role_id int,
+	resource_id int,
+	constraint pk_role_resource primary key (role_id, resource_id)
+);
+
