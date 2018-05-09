373588daa2e09d6a8ceff6556d053a8e3848f55f
Kojima Seiichi <kojima.seiichi@outlook.jp>
2018-05-09 21:54:07 +0900

    hoge

--- src/main/java/jp/tsutajima/people/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package jp.tsutajima.people.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import jp.tsutajima.poeple.entity.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-
-}
